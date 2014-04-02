b290c4eadcc71ff800c272f5dbdd0197790ce7b1
Finn <thefinn93@thefinn93.com>
2014-04-02 14:12:22 -0700

    Dat spelling

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='minatureOctoBatman',
+        name='miniatureOctoBatman',
         packages=find_packages(),
-        namespace_packages=['minatureOctoBatman']
+        namespace_packages=['miniatureOctoBatman']
         )
