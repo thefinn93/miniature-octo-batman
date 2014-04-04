0ec8fc4624cbd33fe66e9ac748ce3bafdb1b4de6
Finn <thefinn93@thefinn93.com>
2014-04-03 23:04:15 -0700

    Quotes are cool

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='miniatureOctoBatman',
     packages=find_packages(),
     namespace_packages=['miniatureOctoBatman'],
-    version=0.1.0,
-    description='Finn's collection of powerline modules',
+    version='0.1.0',
+    description="Finn's collection of powerline modules",
     author='Finn Herzfeld',
     author_email='finn@finn.io',
     url='https://github.com/thefinn93/miniature-octo-batman'
