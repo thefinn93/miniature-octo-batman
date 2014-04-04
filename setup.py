dc1201a5097688ac951de6da21d3cfdc44e9a114
Finn <thefinn93@thefinn93.com>
2014-04-03 23:02:58 -0700

    Added more setup values

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-        name='miniatureOctoBatman',
-        packages=find_packages(),
-        namespace_packages=['miniatureOctoBatman']
-        )
+    name='miniatureOctoBatman',
+    packages=find_packages(),
+    namespace_packages=['miniatureOctoBatman'],
+    version=0.1.0,
+    description='Finn's collection of powerline modules',
+    author='Finn Herzfeld',
+    author_email='finn@finn.io',
+    url='https://github.com/thefinn93/miniature-octo-batman'
+    )
